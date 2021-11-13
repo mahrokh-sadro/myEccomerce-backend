85c7ad19a8ff66e13192eaf925163b9901e6040d
AutoLogon <msadrolodabaee@myseneca.ca>
2021-11-13 13:48:00 -0500

    first commit

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,35 +1,35 @@
 
 
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-// const productSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
 
-//     price: {
-//         type: Number,
-//         required: true
-//     },
-//     description: String,
+    price: {
+        type: Number,
+        required: true
+    },
+    description: String,
 
-//     category: {
-//         type: String,
-//         required: true
-//     },
+    category: {
+        type: String,
+        required: true
+    },
 
-//     quantity: Number,
-//     isBestSeller: Boolean,
-//     isFeatured: Boolean,
-//     photoURL: Array,
-//     dateCreated: {
-//         type: Date,
-//         default: Date.now()
-//     }
-// });
+    quantity: Number,
+    isBestSeller: Boolean,
+    isFeatured: Boolean,
+    photoURL: Array,
+    dateCreated: {
+        type: Date,
+        default: Date.now()
+    }
+});
 
-// const ProductModel = mongoose.model('Product', productSchema);
+const ProductModel = mongoose.model('Product', productSchema);
 
-// module.exports = ProductModel;
+module.exports = ProductModel;
