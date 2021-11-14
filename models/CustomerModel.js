29da70e091f5726415ff775f5eac9a5de1023198
AutoLogon <msadrolodabaee@myseneca.ca>
2021-11-14 09:33:24 -0500

    first commit

--- models/CustomerModel.js
+++ models/CustomerModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 SALT_WORK_FACTOR = 10;
 
@@ -38,35 +38,35 @@ const customerSchema = new Schema({
 
 
 
-// customerSchema.pre("save", function (next) {
-//     var user = this;
+customerSchema.pre("save", function (next) {
+    var user = this;
 
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-//         if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) return next(err);
 
-//         // hash the password using our new salt
-//         bcrypt.hash(user.password, salt, (err, hash) => {
-//             if (err) return next(err);
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
 
-//             // override the cleartext password with the hashed one
-//             user.password = hash;
-//             next();
-//         });
-//     });
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 
 
-// });
+});
 
-// customerSchema.methods.comparePassword = function (candidatePassword, cb) {
-//     bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
+customerSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 
 const CustomerModel = mongoose.model('Customer', customerSchema);
